292c21a0e3aa16394174966b649718d31850cf17
D N Chaturvedi <chaturdn@gmail.com>
2022-03-09 20:43:39 +0530

    Refactored the logic

--- employees/services/auth_service.py
+++ employees/services/auth_service.py
@@ -3,6 +3,7 @@ from django.contrib.auth import authenticate
 
 import requests
 
+
 def loginUser(requestData):
     print("Request DATA: ", requestData)
     is_admin = False
@@ -10,19 +11,19 @@ def loginUser(requestData):
     user = authenticate(
         requestData, username=requestData['username'], password=requestData['password'])
     if user:
-            if not user.is_superuser:
-                employeeID = user.employeeregistration.id
-                if user.is_staff:
-                    is_admin = True   
-            print("User is authenticated")
-            res = requests.post(url='http://127.0.0.1:8000/token/',
-                                data={
-                                    "username": f"{requestData['username']}",
-                                    "password": f"{requestData['password']}"
-                                })
-            token = res.json()
-            print(token)
-            return {'token': token['access'], 'is_admin': is_admin, 'employeeID': employeeID}
+        print("User Object: ", user)
+        #employeeID = user.employeeregistration.id
+        if user.is_superuser:
+            is_admin = True
+        print("User is authenticated")
+        res = requests.post(url='http://127.0.0.1:8000/token/',
+                            data={
+                                "username": f"{requestData['username']}",
+                                "password": f"{requestData['password']}"
+                            })
+        token = res.json()
+        print("Login Token:: ", token)
+        return {'token': token['access'], 'is_admin': is_admin, 'employeeID': employeeID}
     else:
         print("Invalid Cred")
         raise Exception('Invalid Credentials')
