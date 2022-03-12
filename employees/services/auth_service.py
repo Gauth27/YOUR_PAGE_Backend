b119b706f26c234440fae81eb2f926b9750ebc37
Gautham <togautham27@gmail.com>
2022-03-12 19:21:46 +0530

    Updated Logic for Super User

--- employees/services/auth_service.py
+++ employees/services/auth_service.py
@@ -11,19 +11,20 @@ def loginUser(requestData):
     user = authenticate(
         requestData, username=requestData['username'], password=requestData['password'])
     if user:
-        print("User Object: ", user)
-        #employeeID = user.employeeregistration.id
-        if user.is_superuser:
-            is_admin = True
-        print("User is authenticated")
-        res = requests.post(url='http://127.0.0.1:8000/token/',
-                            data={
-                                "username": f"{requestData['username']}",
-                                "password": f"{requestData['password']}"
-                            })
-        token = res.json()
-        print("Login Token:: ", token)
-        return {'token': token['access'], 'is_admin': is_admin, 'employeeID': employeeID}
+            if not user.is_superuser:
+                employeeID = user.employeeregistration.id
+            # print("User Object: ", user)
+            if user.is_staff:
+                is_admin = True
+            print("User is authenticated")
+            res = requests.post(url='http://127.0.0.1:8000/token/',
+                                data={
+                                    "username": f"{requestData['username']}",
+                                    "password": f"{requestData['password']}"
+                                })
+            token = res.json()
+            print("Login Token:: ", token)
+            return {'token': token['access'], 'is_admin': is_admin, 'employeeID': employeeID}
     else:
         print("Invalid Cred")
         raise Exception('Invalid Credentials')
